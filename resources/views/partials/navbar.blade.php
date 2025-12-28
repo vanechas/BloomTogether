b396ca2b35fe117542b36b85e9d6153855b9c165
vanechas <vanessasantoso833@gmail.com>
2025-12-28 22:59:09 +0700

    helpmi

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -27,55 +27,54 @@
                 @auth
                     <li class="nav-item">
                         <a class="nav-link nav-link-bloom {{ request()->routeIs('home') ? 'active' : '' }}"
-                           href="{{ route('home') }}">
-                            Home
-                        </a>
+                           href="{{ route('home') }}">Home</a>
                     </li>
 
                     <li class="nav-item">
                         <a class="nav-link nav-link-bloom {{ request()->routeIs('journal') ? 'active' : '' }}"
-                           href="{{ route('journal') }}">
-                            Journal
-                        </a>
+                           href="{{ route('journal') }}">Journal</a>
                     </li>
 
                     <li class="nav-item">
                         <a class="nav-link nav-link-bloom {{ request()->routeIs('garden') ? 'active' : '' }}"
-                           href="{{ route('garden') }}">
-                            Garden
-                        </a>
+                           href="{{ route('garden') }}">Garden</a>
                     </li>
 
                     <li class="nav-item">
                         <a class="nav-link nav-link-bloom {{ request()->routeIs('friendlist*') ? 'active' : '' }}"
-                           href="{{ route('friendlist') }}">
-                            Friends
-                        </a>
+                           href="{{ route('friendlist') }}">Friends</a>
                     </li>
 
-                    <!-- Profile Dropdown -->
-                    <li class="nav-item dropdown ms-2">
-                        <button
-                            class="nav-link dropdown-toggle d-flex align-items-center profile-dropdown btn btn-link"
-                            type="button"
-                            data-bs-toggle="dropdown"
-                            aria-expanded="false">
+                    <!-- Profile + Dropdown -->
+                    <li class="nav-item dropdown ms-2 d-flex align-items-center">
+
+                        <!-- Avatar / Name → PROFILE PAGE -->
+                        <a href="{{ route('profile') }}"
+                           class="nav-link d-flex align-items-center profile-link">
 
                             @include('partials.pixel-profile', ['size' => 28])
                             <span class="font-body text-wood ms-2 d-none d-md-inline">
                                 {{ auth()->user()->first_name }}
                             </span>
+                        </a>
+
+                        <!-- Caret → DROPDOWN -->
+                        <button
+                            class="btn btn-link nav-link dropdown-toggle dropdown-toggle-split profile-caret"
+                            type="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            <span class="visually-hidden">Toggle Dropdown</span>
                         </button>
 
+                        <!-- Dropdown Menu -->
                         <ul class="dropdown-menu dropdown-menu-end dropdown-bloom">
                             <li>
                                 <a class="dropdown-item" href="{{ route('profile') }}">
                                     Profile
                                 </a>
                             </li>
-
                             <li><hr class="dropdown-divider"></li>
-
                             <li>
                                 <form action="{{ route('logout') }}" method="POST">
                                     @csrf
@@ -89,15 +88,11 @@
 
                 @else
                     <li class="nav-item">
-                        <a class="nav-link nav-link-bloom" href="{{ route('about') }}">
-                            About
-                        </a>
+                        <a class="nav-link nav-link-bloom" href="{{ route('about') }}">About</a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="btn btn-outline-bloom ms-2" href="{{ route('login') }}">
-                            Login
-                        </a>
+                        <a class="btn btn-outline-bloom ms-2" href="{{ route('login') }}">Login</a>
                     </li>
                 @endauth
 
